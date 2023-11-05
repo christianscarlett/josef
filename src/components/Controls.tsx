467798f24820e3fb3f7cd80c3f7cfa71a78daf1c
Christian Scarlett <christianscarlett@Christians-Laptop.local>
2023-11-05 15:16:39 -0500

    move square controls to palette

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,20 +1,19 @@
 import { InputNumber } from "antd";
 import ControlRow from "./ControlRow";
-import PaletteController, { OnPaletteIndexUpdated } from "./PaletteController";
+import PaletteController, {
+  OnPaletteIndexUpdated,
+  OnNumSquaresUpdated,
+} from "./PaletteController";
 import SpacingController, { OnSpacingUpdated } from "./SpacingController";
 
 interface ControlsProps {
   palette: string[];
   verticalSpacing: number;
   horizontalSpacing: number;
-  onNumSquaresUpdated: OnNumSquaresUpdatedFunction;
+  onNumSquaresUpdated: OnNumSquaresUpdated;
   onPaletteIndexUpdated: OnPaletteIndexUpdated;
   onVerticalSpacingUpdated: OnSpacingUpdated;
   onHorizontalSpacingUpdated: OnSpacingUpdated;
-}
-
-interface OnNumSquaresUpdatedFunction {
-  (numSquares: number): void;
 }
 
 function Controls(props: ControlsProps) {
@@ -31,28 +30,12 @@ function Controls(props: ControlsProps) {
   return (
     <div className="flex flex-col items-start">
       <ControlRow
-        title="Squares"
-        controller={
-          <>
-            <InputNumber
-              value={palette.length}
-              min={1}
-              onChange={(n) => {
-                if (n == null) {
-                  n = palette.length;
-                }
-                onNumSquaresUpdated(n);
-              }}
-            />
-          </>
-        }
-      />
-      <ControlRow
         title="Palette"
         controller={
           <PaletteController
             palette={palette}
             onPaletteIndexUpdated={onPaletteIndexUpdated}
+            onNumSquaresUpdated={onNumSquaresUpdated}
           />
         }
       />
--- src/components/PaletteController.tsx
+++ src/components/PaletteController.tsx
@@ -1,17 +1,22 @@
-import { ColorPicker } from "antd";
+import { Button, ColorPicker } from "antd";
 import { ReactNode } from "react";
 
 interface PaletteControllerProps {
   palette: string[];
   onPaletteIndexUpdated: OnPaletteIndexUpdated;
+  onNumSquaresUpdated: OnNumSquaresUpdated;
 }
 
 export interface OnPaletteIndexUpdated {
   (index: number, newColor: string): void;
 }
 
+export interface OnNumSquaresUpdated {
+  (numSquares: number): void;
+}
+
 function PaletteController(props: PaletteControllerProps) {
-  const { palette, onPaletteIndexUpdated } = props;
+  const { palette, onPaletteIndexUpdated, onNumSquaresUpdated } = props;
 
   const pickers: ReactNode[] = palette.map((color, i) => {
     return (
@@ -25,7 +30,29 @@ function PaletteController(props: PaletteControllerProps) {
     );
   });
 
-  return <div className="flex">{pickers}</div>;
+  return (
+    <div className="flex flex-col">
+      <div className="flex">
+        {pickers}
+        <Button
+          type="default"
+          disabled={palette.length <= 1}
+          onClick={() => {
+            let n = Math.max(1, palette.length - 1);
+            onNumSquaresUpdated(n);
+          }}
+        >
+          -
+        </Button>
+        <Button
+          type="default"
+          onClick={() => onNumSquaresUpdated(palette.length + 1)}
+        >
+          +
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default PaletteController;
