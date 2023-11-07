6b9d2a11eb21068cb4bba2b0f7b82e24a365989a
Christian Scarlett <christianscarlett@Christians-Laptop.local>
2023-11-07 00:18:36 -0500

    add textures

--- src/model/Texture.tsx
+++ src/model/Texture.tsx
@@ -1,10 +1,22 @@
 import fabric from "../images/lightfabric.png";
 import paper from "../images/cleanpaper.png";
+import linen from "../images/lightlinen.png";
+import felt from "../images/felt.png";
+import stucco from "../images/stucco.png";
+import snow from "../images/snow.png";
+import wood from "../images/wood.png";
+import noise from "../images/noisy.png";
 
 export enum Texture {
   Flat = "Flat",
   Fabric = "Fabric",
   Paper = "Paper",
+  Linen = "Linen",
+  Felt = "Felt",
+  Stucco = "Stucco",
+  Soft = "Soft",
+  Wood = "Wood",
+  Noise = "Noise",
 }
 
 export interface TextureConfig {
@@ -19,6 +31,24 @@ export const getTextureConfig = function (t: Texture): TextureConfig {
       break;
     case Texture.Paper:
       src = paper;
+      break;
+    case Texture.Linen:
+      src = linen;
+      break;
+    case Texture.Felt:
+      src = felt;
+      break;
+    case Texture.Stucco:
+      src = stucco;
+      break;
+    case Texture.Soft:
+      src = snow;
+      break;
+    case Texture.Wood:
+      src = wood;
+      break;
+    case Texture.Noise:
+      src = noise;
       break;
     default:
       break;
