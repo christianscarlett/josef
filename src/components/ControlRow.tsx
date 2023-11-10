d5c2f804143e1b111d21d31bd423692d2a77f5f8
Christian Scarlett <christianscarlett@Christians-Laptop.local>
2023-11-10 00:59:41 -0500

    add expand chevron

--- src/components/ControlRow.tsx
+++ src/components/ControlRow.tsx
@@ -1,5 +1,9 @@
-import { ReactNode } from "react";
-import { InfoCircleOutlined } from "@ant-design/icons";
+import { ReactNode, useState } from "react";
+import {
+  InfoCircleOutlined,
+  LeftOutlined,
+  DownOutlined,
+} from "@ant-design/icons";
 import { Tooltip } from "antd";
 
 interface ControlRowProps {
@@ -11,6 +15,21 @@ interface ControlRowProps {
 
 function ControlRow(props: ControlRowProps) {
   const { title, controller, tooltipTitle, isMobile } = props;
+
+  const [isExpanded, setIsExpanded] = useState(true);
+
+  const expandIcon = isExpanded ? (
+    <DownOutlined
+      className="ml-2 opacity-70 hover:opacity-100 hover:text-black hover:cursor-pointer"
+      onClick={() => setIsExpanded(false)}
+    />
+  ) : (
+    <LeftOutlined
+      className="ml-2 opacity-70 hover:opacity-100 hover:text-black hover:cursor-pointer"
+      onClick={() => setIsExpanded(true)}
+    />
+  );
+
   return (
     <div
       className={
@@ -19,19 +38,29 @@ function ControlRow(props: ControlRowProps) {
         " items-center justify-center bg-white/30 p-3 rounded backdrop-blur-sm drop-shadow-2xl mb-5 w-full"
       }
     >
-      <div className="flex items-center justify-center">
-        <h3>{title}</h3>
-        <Tooltip title={tooltipTitle}>
-          <InfoCircleOutlined className="ml-2 opacity-70 hover:opacity-100 hover:text-black hover:cursor-pointer" />
-        </Tooltip>
-      </div>
       <div
-        className={
-          "border-solid border-l border-b border-black self-stretch " +
-          (isMobile ? "my-3" : "mx-3")
-        }
-      ></div>
-      {controller}
+        className={isMobile ? "flex items-center justify-around w-full" : ""}
+      >
+        {isMobile && <div className="opacity-0">{expandIcon}</div>}
+        <div className="flex items-center justify-center">
+          <h3>{title}</h3>
+          <Tooltip title={tooltipTitle}>
+            <InfoCircleOutlined className="ml-2 opacity-70 hover:opacity-100 hover:text-black hover:cursor-pointer" />
+          </Tooltip>
+        </div>
+        {isMobile && expandIcon}
+      </div>
+      {(!isMobile || isExpanded) && (
+        <>
+          <div
+            className={
+              "border-solid border-l border-b border-black self-stretch " +
+              (isMobile ? "my-3" : "mx-3")
+            }
+          ></div>
+          {controller}
+        </>
+      )}
     </div>
   );
 }
